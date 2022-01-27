54c463c2d5e5d70479d6576eb40127daa2bdb8aa
Naveenchikka <35519706+Naveenchikka@users.noreply.github.com>
2022-01-27 18:53:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-var apiurl = "http://www.omdbapi.com/?apikey=345660a5";
-
-
-
+var keyword;
+var movie;
 async function getdata()
 {
 
     try {
-		let keyword = document.getElementById('search').value;
+		keyword = document.getElementById('search').value;
 		// let year = document.getElementById('y').value;
 		// var e = document.getElementById('p');
 		// console.log(e);
@@ -21,9 +19,10 @@ async function getdata()
 				"x-rapidapi-key": "1b2390d925mshe2e632937d32b9dp10381ejsn3a3540128cb3"
 			}
 		});
-		var movie = await response.json()
+		movie = await response.json()
 		console.log(movie)
 		console.log(movie.value);
+		console.log("status = "+movie.statuscode);
 
 		if(movie.Response != 'False')
 		{
@@ -40,7 +39,8 @@ async function getdata()
 		// if(movie)
 		//     document.getElementsByClassName('card').style.property= 'visible';
 	  } catch (err) {
-		console.error(err)
+		console.error(err);
+		document.getElementById('err').innerHTML = "Server Error, Please search again";
 	  }
 
 }
@@ -52,8 +52,15 @@ async function getdata()
 // 	document.getElementById('p').selectedIndex = '';
 // }
 
+function dosomeFunction()
+{
+  createPost(movie);
+}
+
 function createPost(movie)
 { 
+	document.getElementById('err').style.display = 'hidden';
+    document.getElementById('key').innerHTML = "News About "+keyword;
 	var fragment  = new DocumentFragment();
    
  for(let i=0;i<movie.value.length;i++)
@@ -63,7 +70,7 @@ function createPost(movie)
 
 	 var newDiv = document.createElement("div");
 	 newDiv.id = 'r'+i;
-     newDiv.className = 'blocks';
+     newDiv.className = 'blocks .col-md';
 	//  newDiv.style.display = 'block';
 	newDiv.style.display = 'block';
 	
@@ -99,18 +106,20 @@ function createPost(movie)
 
 	 var newDiv1 = document.createElement("div");
 	 newDiv1.id = 'right'+i;
-     newDiv1.className = 'blocks1';
+     newDiv1.className = 'blocks1 .col-sm';
 	 newDiv1.style.display = 'block';
 
 	 var providerImg = document.createElement("img");
 	 providerImg.id = 'providerImg'+i;
 	 providerImg.className = 'providerImg';
      providerImg.src = movie.value[i].image.url;
-
+     
+	 
 	 newDiv1.appendChild(providerImg);
      fragDivs.append(newDiv,newDiv1);
 
 	 fragDivs.id = 'SideBySide';
+	 fragDivs.className = 'card w-75';
 	//  fragDivs.style.display ='flex';
 	fragment.appendChild(fragDivs);
 	 
@@ -141,3 +150,5 @@ function createPost(movie)
 //   }
 
 }
+
+
